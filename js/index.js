f07293cbf7d946e7b3bf3812e8474a9506bb2d45
shahzadakram786 <alyakram786@gmail.com>
2024-08-19 20:22:36 +0500

    passowrd generator project done

---
+++ js/index.js
@@ -0,0 +1,39 @@
+const lowerCaseLetter = "abcdefghijklmnopqrstuvwxyz";
+const upperCaseLetter = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const numbers = "0123456789";
+const symbols = "!@#$%^&*()_+-={}[]|;,./><?";
+
+const length1 = document.getElementById("length");
+const lowercase1 = document.getElementById("lowercase");
+const uppercase2 = document.getElementById("uppercase");
+const number2 = document.getElementById("number");
+const symbols1 = document.getElementById("symbols");
+const generateBtn = document.getElementById("generateP");
+const password1 = document.getElementById("password");
+
+generateBtn.addEventListener("click", function() {
+    const length = length1.value;
+    let character = "";
+    let password = "";
+
+    if (lowercase1.checked) {
+        character += lowerCaseLetter;
+    }
+
+    if (uppercase2.checked) {
+        character += upperCaseLetter;
+    }
+
+    if (number2.checked) {
+        character += numbers;
+    }
+
+    if (symbols1.checked) {
+        character += symbols;
+    }
+
+    for (let i = 0; i < length; i++) {
+        password += character.charAt(Math.floor(Math.random() * character.length));
+        password1.value = password;
+    }
+});
